7830b4bef5ed623e8f7db130d8d066566e966863
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2019-01-08 15:06:26 -0400

    Tested food2fork api calls with Axios

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,17 @@
 // Global app controller
-import x from './test';
-const y = 2666;
-console.log(`${x} to ${x}`);
+import axios from 'axios';
+
+async function getResults(query) {
+    const key = 'fb1d52c867654a2b1708d52be8f5b554';
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    try{
+        const res = await axios(
+            `${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`
+        );
+        const recipes = res.data.recipes[0];
+        console.log(recipes);
+    } catch (err) {
+        alert(err);;
+    };
+};
+getResults('pizza');
