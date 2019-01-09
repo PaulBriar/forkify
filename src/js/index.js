1e4ce5f9f6c6cd98a5c691a67eb749417dc2e120
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2019-01-09 16:18:01 -0400

    Added renderRecipe to add recipe to UI

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,30 @@
 // Global app controller
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 //Global state of app
 const state = {};
 
 const controlSearch = async () => {
     //Get query from view
-    const query = 'pizza';
+    const query = searchView.getInput();
 
     if (query) {
         //New search obj, add to state
         state.search = new Search(query);
         //Prepare UI for results
-
+        searchView.clearInput();
+        searchView.clearResults();
         //Search for receipes
         await state.search.getResults();
         //Render results on UI
-        console.log(state.search.result);
-    }
-}
+        searchView.renderResults(state.search.result);
+    };
+};
 
-document.querySelector('.search').addEventListener('submit', (event) => {
+elements.searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
     controlSearch();
-})
+});
 
