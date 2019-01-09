430455b6415ceeccd8faf6fc1618edd81147337b
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2019-01-09 09:11:00 -0400

    Added search controller & state management

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,27 @@
 // Global app controller
 import Search from './models/Search';
 
-const search = new Search('pizza');
+//Global state of app
+const state = {};
 
-console.log(search);
+const controlSearch = async () => {
+    //Get query from view
+    const query = 'pizza';
 
-search.getResults();
+    if (query) {
+        //New search obj, add to state
+        state.search = new Search(query);
+        //Prepare UI for results
+
+        //Search for receipes
+        await state.search.getResults();
+        //Render results on UI
+        console.log(state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', (event) => {
+    event.preventDefault();
+    controlSearch();
+})
+
